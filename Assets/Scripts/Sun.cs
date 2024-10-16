232b16dce493a54e1d82fbeb51205375bd3f07a1
Raptor <panraptor13@gmail.com>
2024-10-16 18:54:33 +0200

    Quick fix

--- Assets/Scripts/Sun.cs
+++ Assets/Scripts/Sun.cs
@@ -8,7 +8,6 @@ public class Sun : MonoBehaviour
 {
     [SerializeField] private PlayerDamage player;
     [SerializeField] private Collider playerCollider;
-    //[SerializeField] private DOTweenPath path;
     [SerializeField] private float lightAngle;
     [SerializeField] private GameObject sunlight;
     [SerializeField] private LayerMask terrainLayer;
@@ -17,8 +16,6 @@ public class Sun : MonoBehaviour
     private float timer;
     private void Start()
     {
-        //path.DOPlay();
-        //night = false;
         StartCoroutine(DayNightCycle());
     }
 
@@ -28,20 +25,13 @@ public class Sun : MonoBehaviour
         Ray ray = new Ray(player.transform.position, -sunAngle);
         RaycastHit hit;
 
-        if (Physics.Raycast(ray, out hit, Mathf.Infinity, terrainLayer)) // to też nie działa, coś jest nei tak
+        if (Physics.Raycast(ray, out hit, Mathf.Infinity, terrainLayer)) 
         {
             if (hit.collider || night)
             {
                 player.exposed = false;
                 Debug.DrawRay(player.transform.position, -sunAngle * 100, Color.green);
-                //Debug.Log(hit.collider);
-                //Debug.Log(night);
             }
-            // else if (hit.collider == null && !night) 
-            // {
-            //     player.exposed = true;
-            //     Debug.DrawRay(player.transform.position, -sunAngle * 100, Color.red);
-            // } 
         }
         else if (!night)
         {
@@ -49,26 +39,6 @@ public class Sun : MonoBehaviour
             Debug.DrawRay(player.transform.position, -sunAngle * 100, Color.red);
         }
     }
-    /*private void Update()
-        {
-            Vector3 playerDirection = -transform.position + (player.transform.position + new Vector3(0, 1, 0));
-            Ray ray = new Ray(transform.position, playerDirection * 100);
-            RaycastHit hit;
-            
-            if (Physics.Raycast(ray, out hit, Mathf.Infinity))
-            {
-                if (hit.collider == playerCollider && !night)
-                {
-                    player.exposed = true;
-                    Debug.DrawRay(transform.position, playerDirection * 100, Color.red);
-                }
-                else if (hit.collider != playerCollider || night)
-                {
-                    player.exposed = false;
-                    Debug.DrawRay(transform.position, playerDirection * 100, Color.green);
-                }
-            }
-        }*/
 
     IEnumerator DayNightCycle()
     {
@@ -79,42 +49,15 @@ public class Sun : MonoBehaviour
             while (timer < 15)
             {
                 timer += Time.deltaTime;
-               // Debug.Log(timer);
                 var t = timer / 15;
-                lightAngle = Mathf.LerpAngle(45.0f, 135.0f, t); //nie
-                // lightAngle += 6; też robi syf
+                lightAngle = Mathf.LerpAngle(45.0f, 135.0f, t);
                 yield return null;
-                sunlight.transform.rotation = Quaternion.Euler(lightAngle, 0f, 0f); //niepoprawne cyferki, skacze wszędzie ale nie tam gdzie trzeba
-                //sunlight.transform.Rotate(Vector3.right, 6); //nie jest gładko ALE DZIAŁA
-                //lightAngle = sunlight.transform.rotation.eulerAngles.x;
-                //Debug.Log(lightAngle);
-                //yield return new WaitUntil(() => Input.GetKeyDown(KeyCode.E));
+                sunlight.transform.rotation = Quaternion.Euler(lightAngle, 0f, 0f);
             }
             night = true;
             sunlight.SetActive(false);
             yield return new WaitForSeconds(15f);
             timer = 0;
-            //sunlight.transform.Rotate(Vector3.right, -90); 
         }
     }
-    
-    /*public void Restart()
-    {
-        StartCoroutine(Finished());
-    }
-    IEnumerator Finished()
-    {
-        path.DORestart();
-        path.DOPause();
-        timer = 0;
-        night = true;
-        while (timer < 15)
-        {
-            timer += 1;
-            Debug.Log(timer);
-            yield return new WaitForSeconds(1f);
-        }
-        night = false;
-        path.DOPlay();
-    }*/
 }
